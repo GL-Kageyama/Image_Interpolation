f71ae3d18664658f74a71fefdae17096d9264a10
a1kageyama <36861752+a1kageyama@users.noreply.github.com>
2019-10-26 14:32:51 +0900

    BiLinear method and BiCubic method.

--- Image_Interpolation.py
+++ Image_Interpolation.py
@@ -1,7 +1,7 @@
 #==================================================================
 #-----------------      Image Interpolation      ------------------
 #==================================================================
-# Pattern 1 : Basic Image in Python
+#                        Basic Image 
 #------------------------------------------------------------------
 # Use the algorithm to create a dot picture before interpolation.
 #==================================================================
@@ -15,12 +15,12 @@ z = np.cos(10*(x**2 + y**2)) * np.sin(10*(x**2 + y**2))
 plt.figure()
 plt.pcolor(x, y, z, cmap="jet")
 plt.colorbar()
-plt.savefig("Besic_Python_Image.png", dpi=300)
+plt.savefig("Besic.png", dpi=300)
 plt.show()
 plt.close()
 
 #==================================================================
-# Pattern 2 : Interpolation Image in BiLinear method
+#                      BiLinear method
 #------------------------------------------------------------------
 # The BiLinear method is an interpolation method that 
 # expands the Linear method variables into two dimensions.
@@ -31,6 +31,7 @@ from scipy.interpolate import interp2d
 x, y = np.mgrid[-1:1:20j, -1:1:20j]
 z = np.cos(10*(x**2 + y**2)) * np.sin(10*(x**2 + y**2))
 
+# This is BiLinear method Point.
 tck = interp2d(x, y, z, kind="linear")
 
 xnew, ynew = np.mgrid[-1:1:200j, -1:1:200j]
@@ -39,17 +40,21 @@ znew = tck(xnew[:, 0], ynew[0, :])
 plt.figure()
 plt.pcolor(xnew, ynew, znew, cmap="jet")
 plt.colorbar()
-plt.savefig("BiLinear_Image.png", dpi=300)
+plt.savefig("BiLinear.png", dpi=300)
 plt.show()
 plt.close()
 
 #==================================================================
-# Pattern 3 : Interpolation Image in BiCubic method
+#                       BiCubic method
+#------------------------------------------------------------------
+# The BiCubic method is an interpolation method that extends 
+# the one-dimensional Cubic method.
 #==================================================================
 
 x, y = np.mgrid[-1:1:20j, -1:1:20j]
 z = np.cos(10*(x**2 + y**2)) * np.sin(10*(x**2 + y**2))
 
+# This is BiCubic method Point.
 tck = interp2d(x, y, z, kind="cubic")
 
 xnew, ynew = np.mgrid[-1:1:200j, -1:1:200j]
@@ -58,7 +63,7 @@ znew = tck(xnew[:, 0], ynew[0, :])
 plt.figure()
 plt.pcolor(xnew, ynew, znew, cmap="jet")
 plt.colorbar()
-plt.savefig("BiCubic_Image.png", dpi=300)
+plt.savefig("BiCubic.png", dpi=300)
 plt.show()
 plt.close()
 
